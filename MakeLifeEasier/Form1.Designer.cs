cd9b3ef40fd890ba154cc49f9dfcfd4c938d7bb6
shuming <zsmingwh@163.com>
2018-08-01 10:39:02 +0800

    init

---
+++ MakeLifeEasier/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace MakeLifeEasier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseInputButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputExcelPathLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // BrowseInputButton
+            // 
+            this.BrowseInputButton.Location = new System.Drawing.Point(271, 37);
+            this.BrowseInputButton.Name = "BrowseInputButton";
+            this.BrowseInputButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseInputButton.TabIndex = 1;
+            this.BrowseInputButton.Text = "浏览";
+            this.BrowseInputButton.UseVisualStyleBackColor = true;
+            this.BrowseInputButton.Click += new System.EventHandler(this.BrowseInputButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(271, 89);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "开始生成";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "导出文件:";
+            // 
+            // InputExcelPathLabel
+            // 
+            this.InputExcelPathLabel.AutoSize = true;
+            this.InputExcelPathLabel.Location = new System.Drawing.Point(93, 42);
+            this.InputExcelPathLabel.MaximumSize = new System.Drawing.Size(180, 20);
+            this.InputExcelPathLabel.Name = "InputExcelPathLabel";
+            this.InputExcelPathLabel.Size = new System.Drawing.Size(11, 12);
+            this.InputExcelPathLabel.TabIndex = 6;
+            this.InputExcelPathLabel.Text = "-";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "\"Excel文件|*.xls;*.xlsx\"";
+            this.openFileDialog1.InitialDirectory = "Environment.GetFolderPath(Environment.SpecialFolder.Desktop";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(30, 91);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.Filter = "\"Excel文件|*.xls;*.xlsx\"";
+            this.openFileDialog2.InitialDirectory = "Environment.GetFolderPath(Environment.SpecialFolder.Desktop";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 136);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.InputExcelPathLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.BrowseInputButton);
+            this.Name = "Form1";
+            this.Text = "MakeLifeEasier_v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BrowseInputButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label InputExcelPathLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ MakeLifeEasier/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+using NPOI.SS.UserModel;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MakeLifeEasier
+{
+    public partial class Form1 : Form
+    {
+        private string inputExcelPath, outputExcelPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseInputButton_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog1.Filter = "Excel文件|*.xls;*.xlsx";
+                                                        
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                inputExcelPath = openFileDialog1.FileName;
+                InputExcelPathLabel.Text = Path.GetFileName(inputExcelPath);
+            }
+        }
+
+        /*
+        private void BrowseOutputButton_Click(object sender, EventArgs e)
+        {
+            //openFileDialog2.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog2.Filter = "Excel文件|*.xls;*.xlsx";
+
+            if (this.openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                outputExcelPath = openFileDialog2.FileName;
+                OutputExcelPathLabel.Text = Path.GetFileName(outputExcelPath);
+            }
+        }
+        */
+
+        private List<List<String>> GetRestDateofThisMonth()
+        {
+            DateTime startDate = this.dateTimePicker1.Value;
+            List<List<String>> dateRange = new List<List<String>>();
+
+            DateTime d1 = new DateTime(startDate.Year, startDate.Month, 1);
+            DateTime endDate = d1.AddMonths(1).AddDays(-1);
+            TimeSpan span = endDate.Subtract(startDate);
+            int dayDiff = span.Days + 1;
+            if (startDate.Day == endDate.Day)
+            {
+                dayDiff = 0;
+            }
+            DateTime newDate = new DateTime();
+            for (int i = 0; i <= dayDiff; i++)
+            {
+                newDate = startDate.AddDays(i);
+                string[] dateStrl = { newDate.ToString("yyyy/M/d 0:00"), newDate.ToString("yyyy/M/d 12:00") };
+                dateRange.Add(new List<string>(dateStrl));
+                Console.WriteLine(dateStrl[0]);
+            }
+            return dateRange;
+        }
+
+        private List<List<String>> GetInputData()
+        {
+            FileStream fileStream = new FileStream(inputExcelPath, FileMode.Open, FileAccess.Read);
+
+            IWorkbook inputExcel = null;
+            if (inputExcelPath.IndexOf(".xlsx") > 0) // 2007
+            {
+                inputExcel = new XSSFWorkbook(fileStream);
+            }
+            else if (inputExcelPath.IndexOf(".xls") > 0) // 2003
+            {
+                inputExcel = new HSSFWorkbook(fileStream);
+            }
+
+            ISheet sheet = inputExcel.GetSheetAt(0);
+            IRow row;
+
+            // Get the column number of city, town, district
+            row = sheet.GetRow(0);
+            int columnNum = row.LastCellNum;
+            List<String> titles = new List<String>();
+            for (int i = 0; i < columnNum; i++)
+            {
+                titles.Add(row.GetCell(i).ToString());
+            }
+            int cityIndex = titles.FindIndex(a=>a=="地市");
+            int townIndex = titles.FindIndex(a => a == "区县");
+            int districtIndex = titles.FindIndex(a => a == "小区");
+   
+
+            List<List<String>> data = new List<List<String>>();
+            for (int i = 1; i < sheet.LastRowNum; i++)  // Every row except the first
+            {
+                row = sheet.GetRow(i);
+                if (row != null)
+                {
+                    string[] rowData = { row.GetCell(cityIndex).ToString(), row.GetCell(townIndex).ToString(), row.GetCell(districtIndex).ToString() };
+                    data.Add(new List<String>(rowData));
+
+                }
+            }
+
+            fileStream.Close();
+            inputExcel.Close();
+
+            return data;
+        }
+
+        private List<List<String>> ConvertData(List<List<String>> data, List<List<String>> dateRange)
+        {
+            List<List<String>> newData = new List<List<String>>();
+            foreach (List<String> date in dateRange)
+            {
+                foreach (List<String> t in data)
+                {
+                    String[] rowData ={ "聚类市场断电", "工程", date[0], date[1], t[2], t[0], t[1] };
+                    newData.Add(new List<String>(rowData));
+                }
+            }
+            return newData;
+        }
+
+        private List<List<String>> GetOutputData()
+        {
+            List<List<String>> data = new List<List<String>>();
+
+            if (outputExcelPath == null)
+            {
+                return data;
+            }
+
+            FileStream fileStream = new FileStream(outputExcelPath, FileMode.Open, FileAccess.Read);
+
+            IWorkbook outputExcel = null;
+            if (outputExcelPath.IndexOf(".xlsx") > 0) // 2007
+            {
+                outputExcel = new XSSFWorkbook(fileStream);
+            }
+            else if (outputExcelPath.IndexOf(".xls") > 0) // 2003
+            {
+                outputExcel = new HSSFWorkbook(fileStream);
+            }
+
+            ISheet sheet = outputExcel.GetSheetAt(0);
+            IRow row;
+            ICell cell;
+
+            for (int i = 1; i < sheet.LastRowNum; i++)  // Every row except the first
+            {
+                row = sheet.GetRow(i);
+                if (row != null)
+                {
+                    List<String> rowData = new List<string>();
+                    for (int j = 0; j < 7; j++)
+                    {
+                        cell = row.GetCell(j);
+                        if (cell.CellType == CellType.String)
+                        {
+                            rowData.Add(cell.ToString());
+                        }
+                        else
+                        {
+                            rowData.Add(cell.DateCellValue.ToString("yyyy/m/d h:mm"));
+                        }
+                        
+                    }
+                    data.Add(new List<String>(rowData));
+                }
+            }
+
+            fileStream.Close();
+            outputExcel.Close();
+
+            return data;
+        }
+
+        private void WriteToNewFile(List<List<String>> data)
+        {
+            XSSFWorkbook newWorkbook = new XSSFWorkbook();  
+            newWorkbook.CreateSheet("Sheet1");
+            ISheet sheet = newWorkbook.GetSheetAt(0);
+            sheet.SetColumnWidth(2, 20 * 256);
+            sheet.SetColumnWidth(3, 20 * 256);
+
+            sheet.CreateRow(0);
+            IRow row = sheet.GetRow(0);
+            List<ICell> cells = new List<ICell>();
+            for (int j = 0; j < 7; j++)
+            {
+                cells.Add(row.CreateCell(j));
+            }
+            cells[0].SetCellValue("割接名称");
+            cells[1].SetCellValue("任务类型");
+            cells[2].SetCellValue("开始时间");
+            cells[3].SetCellValue("结束时间");
+            cells[4].SetCellValue("小区名称");
+            cells[5].SetCellValue("地市");
+            cells[6].SetCellValue("区县");
+
+            IDataFormat dataformat = newWorkbook.CreateDataFormat();
+            ICellStyle style0 = newWorkbook.CreateCellStyle();
+            style0.DataFormat = dataformat.GetFormat("yyyy/m/d h:mm");
+
+            for (int i = 1; i < data.Count + 1; i++)
+            {
+                sheet.CreateRow(i);
+                row = sheet.GetRow(i);
+                for (int j = 0; j < 7; j++)
+                {
+                    ICell cell = row.CreateCell(j);
+                    if (j == 2 || j == 3)
+                    {
+                        cell.SetCellValue(Convert.ToDateTime(data[i - 1][j]));
+                        cell.CellStyle = style0;
+                    }else{
+                        cell.SetCellValue(data[i - 1][j]);
+                    }
+                }
+            }
+
+            FileStream newFile = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+ @"\MakeLifeEasier.xlsx", FileMode.Create);
+            newWorkbook.Write(newFile);
+            newFile.Close();
+            newWorkbook.Close();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (inputExcelPath == null)
+            {
+                return;
+            }
+            List<List<String>> dateRange = this.GetRestDateofThisMonth();
+
+            List<List<String>> inputData = this.GetInputData();
+
+            List<List<String>> newData = this.ConvertData(inputData, dateRange);
+
+            List<List<String>> dataToMerge = this.GetOutputData();
+
+            dataToMerge.AddRange(newData);
+
+            WriteToNewFile(dataToMerge);
+
+            MessageBox.Show("MakeLifeEasier.xlsx 已在桌面生成", "生成成功");
+        }
+
+    }
+}
